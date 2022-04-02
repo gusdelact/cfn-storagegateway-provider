b7c70694a5e625200ad37cef547e989089ccd43f
Gustavo De la Cruz <gusdelact@gmail.com>
2022-04-02 00:00:40 +0000

    Modificado deploy.sh para darle formato

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
-aws cloudformation create-stack  --stack-name cfn-storagegateway-provider-lambda --template-body file://lambda.yaml  --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --parameters  \
+aws cloudformation create-stack  --stack-name cfn-storagegateway-provider-lambda  \
+	--template-body file://lambda.yaml  \
+	--capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --parameters  \
 	ParameterKey=LambdaName,ParameterValue=cfn-storagegateway-provider  \
 	ParameterKey=DeployBucket,ParameterValue=cfn-storagegateway-provider-deploy-721695533642  \
 	ParameterKey=LambdaZipFile,ParameterValue=cfn-storagegateway-provider.zip
